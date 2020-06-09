6fe785fb4b3275c2faddd2792cac86245bba84e5
jaroslawmalekcodete <jaroslaw.malek@codete.com>
2020-06-09 13:58:39 +0200

    refactoring related with installing many kernels in one conda env

--- java-dist/conda_recipe/post-link.bat
+++ java-dist/conda_recipe/post-link.bat
@@ -1,5 +1,5 @@
 @echo off
 (
   REM Uninstall BeakerX notebook extension
-  "%PREFIX%\Scripts\beakerx-kernel-java.exe" "install"
+  "%PREFIX%\Scripts\beakerx_kernel_java.exe" "install"
 ) >>"%PREFIX%\.messages.txt" 2>&1
